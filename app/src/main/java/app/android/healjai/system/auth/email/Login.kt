a07469d9ba54e9d96ec80389de035629afa10052
chatchai1996 <110679183+chatchai1996@users.noreply.github.com>
2023-04-15 10:17:14 +0700

    16/4/2566 16:35 - add directories to plan the  program system

--- app/src/main/java/app/android/healjai/system/auth/email/Login.kt
+++ app/src/main/java/app/android/healjai/system/auth/email/Login.kt
@@ -1,41 +1,43 @@
-package app.android.healjai
+package app.android.healjai.system.auth.email
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import app.android.healjai.ui.theme.HealJaiTheme
-class MainActivity : ComponentActivity() {
+import app.android.healjai.system.auth.email.ui.theme.HealJaiTheme
+
+class Login : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HealJaiTheme {
+                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun DefaultPreview2() {
     HealJaiTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colors.background
-        ) {
-
-        }
+        Greeting("Android")
     }
 }
